5acbadedf67588499d01a1a0970a77d62bc6512b
JohnFonsecs <jvfc2000@hotmail.com>
2024-10-16 16:30:20 -0300

    Refatorar classes Iphone e Nokia
    - Herdar da classe Smartphone
    - Sobrescrever o método "InstalarAplicativo"
    - Implementar as propriedades faltantes de acordo com o diagrama
    - Passar os parâmetros do construtor para as propriedades
    - Realizar os testes com as classes Nokia e Iphone

--- Program.cs
+++ Program.cs
@@ -1,3 +1,10 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Smartphone nokia = new Nokia("1234567890", "Nokia 3310", "BBBB1234", 16);
+Smartphone iphone = new Iphone("1111111111", "Iphone 13", "DDDDD1234", 64);
+
+nokia.InstalarAplicativo("Instagram");
+iphone.InstalarAplicativo("Facebook");
+
+nokia.Ligar();
+iphone.ReceberLigacao();
